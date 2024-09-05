2a9b8f6e134a7ab55382910612723c57e2dfbad0
Victor Zanesco Moro <86382853+ViiczZanes@users.noreply.github.com>
2024-09-05 00:24:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const puppeteer = require('puppeteer');
 (async () => {
   // Lançar o navegador
   const browser = await puppeteer.launch({
-    headless: true,  // Modo headless
-    args: ['--no-sandbox', '--disable-setuid-sandbox']  // Argumentos para desativar o sandbox
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
+
+  // Definir cabeçalhos personalizados
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive'
+  });
 
   // URL da página que você deseja acessar
   const url = 'https://www.glassdoor.com.br/Avaliações/JTI-Japan-Tobacco-International-Avaliações-E6359.htm';
 
-  // Interceptar e capturar a resposta
-  page.on('response', response => {
-    if (response.url() === url) {
-      console.log(`Código de resposta: ${response.status()}`);
-    }
-  });
-
-  // Acessar a página e capturar o código de resposta
-  const response = await page.goto(url);
+  // Acessar a página
+  const response = await page.goto(url, { waitUntil: 'networkidle2' });
   console.log(`Código de resposta após goto: ${response.status()}`);
 
   // Usar um timeout para garantir que a página tenha tempo para carregar
-  await new Promise(resolve => setTimeout(resolve, 5000));  // Aguarda 5 segundos
+  await new Promise(resolve => setTimeout(resolve, 5000));
 
   // Obter o conteúdo HTML da página
   const htmlContent = await page.content();
